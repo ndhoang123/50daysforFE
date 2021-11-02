d422e8abbec75d66b4cc29828c7b83c1424e361c
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-02 10:45:54 +0700

    [Day 9 - js] Get value by using async/await

--- Day9/script.js
+++ Day9/script.js
@@ -1,11 +1,29 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-jokeBtn.addEventListener('click', generateJoke);
+jokeBtn.addEventListener('click', generateJokeAsync)
 
-generateJoke()
+generateJokeAsync()
 
-function generateJoke() {
+// Normal fetch
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     const url = 'https://icanhazdadjoke.com'
+
+//     fetch(url, config)
+//         .then(res => res.json())
+//         .then(data => {
+//             jokeEl.innerHTML = data.joke
+//         });
+// }
+
+// async/await
+async function generateJokeAsync(){
     const config = {
         headers: {
             'Accept': 'application/json'
@@ -14,9 +32,9 @@ function generateJoke() {
 
     const url = 'https://icanhazdadjoke.com'
 
-    fetch(url, config)
-        .then(res => res.json())
-        .then(data => {
-            jokeEl.innerHTML = data.joke
-        });
+    const res = await fetch(url, config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
 }
